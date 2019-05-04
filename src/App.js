9c8251b8d356668090b87df41a76b66187380e4c
JJ <jenjones@ucdavis.edu>
2019-05-04 11:57:36 -0700

    update

--- src/App.js
+++ src/App.js
@@ -3,24 +3,36 @@ import images from "./images.json";
 import Wrapper from "./components/Wrapper/Wrapper";
 import Header from "./components/Header/Header";
 import Jumbotron from "./components/Jumbotron/Jumbotron";
-import Images from "./components/Images/Images";
+import ImageCard from "./components/ImageCard/ImageCard";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 class App extends React.Component {
   state = {
-    images,
+    images: images,
     clickedImages: [],
-    highscore: 0,
+    highScore: 0,
     score: 0,
-    totalClick: 0
+    totalClick: 0,
+    displayMessage: ""
   };
 
   handleShuffle(id) {
     if (!this.state.clickedImages.includes(id)) {
       this.setState({
         score: this.state.score + 1,
-        clickedImages: [...this.state.clickedImages, id]
+        displayMessage: this.state.score + 1 === 12 ? "You won!" : "",
+        clickedImages: [...this.state.clickedImages, id],
+        highScore:
+          this.state.score + 1 > this.state.highScore
+            ? this.state.score + 1
+            : this.state.highScore
+      });
+    } else {
+      this.setState({
+        score: 0,
+        displayMessage: "Bummer! You lost. Try again?",
+        clickedImages: []
       });
     }
     this.setState({
@@ -32,11 +44,15 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <Header score />
+        <Header
+          score={this.state.score}
+          highScore={this.state.highScore}
+          msg={this.state.displayMessage}
+        />
         <Jumbotron />
         <Wrapper>
           {this.state.images.map(i => (
-            <Images
+            <ImageCard
               key={i.id}
               name={i.name}
               image={i.image}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,17 @@
 import React from "react";
 import "./header.css";
 
-function Header() {
+function Header(props) {
   return (
     <nav className="navbar">
       <a className="navbar-brand" href="#">
-        <h2 className="ml-5">
-          Clicky Game
-        </h2>
+        <h2 className="ml-5">Clicky Game</h2>
       </a>
 
-      <div className="guess text-center">
-        How good is your memory?
-      </div>
+      <div className="guess text-center">{props.msg}</div>
 
       <div className="guess mr-5">
-        Score: | Top Score:
+        Score: {props.score} | Top Score: {props.highScore}
       </div>
     </nav>
   );
